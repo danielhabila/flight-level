eee867d64df6f6cab3b73e2385e56716b7260411
danielhabila <100402238+danielhabila@users.noreply.github.com>
2023-09-08 09:54:41 -0700

    #

--- client/src/pages/AddSalary.jsx
+++ client/src/pages/AddSalary.jsx
@@ -1,35 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Disclosure } from "@headlessui/react";
-import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
 import Header from "../partials/Header";
 import Footer from "../partials/Footer";
-import axios from "axios";
-import BgSVG from "../partials/ui/bgSVG";
+import BgSVG from "../partials/ui/BgSVG";
 import SalaryForm from "../partials/SalaryForm";
 
-export default function Example() {
-  const [firstOfficer, setFirstOfficer] = useState(true);
-  const [salaryList, setSalaryList] = useState([]);
-  //   const [salaryDetails, setSalaryDetails] = useState(true);
-  const [loading, setLoading] = useState(false);
-
-  /* ************************************************************************* */
-  useEffect(() => {
-    const fetchSalary = async () => {
-      try {
-        setLoading(true);
-        const res = await axios.get("/api/");
-        setSalaryList(res.data);
-        console.log("salaryList ", salaryList);
-        setLoading(false);
-      } catch (err) {
-        console.error(err);
-        setLoading(false);
-      }
-    };
-    fetchSalary();
-  }, []);
-
+export default function AddSalary() {
   return (
     <div className="relative isolate overflow-hidden bg-gray-900">
       <BgSVG />
